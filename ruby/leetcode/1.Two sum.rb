4f7a7701e20acb2862d12ddbc95ab9a023ad59cb
Simple0109 <p.cscscs@icloud.com>
2023-10-12 14:41:18 +0900

    leetcode, method

--- ruby/leetcode/1.Two sum.rb
+++ ruby/leetcode/1.Two sum.rb
@@ -2,5 +2,11 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-
+  dict = {}
+  nums.each_with_index do |item, i|
+    if dict[target - item]
+      return dict[target - item], i
+    end
+    dict[item] = 1
+  end
 end
