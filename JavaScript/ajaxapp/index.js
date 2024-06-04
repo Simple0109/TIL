f7df381b49bb1ec93f5ff1d52bb5a23076d53f4f
ishikawa <arcv@ishikawanoMacBook-Air.local>
2024-06-04 18:12:54 +0900

    fetchUserInfoの分割

--- JavaScript/ajaxapp/index.js
+++ JavaScript/ajaxapp/index.js
@@ -1,26 +1,18 @@
+function main(){
+    fetchUserInfo("js-primer-example");
+}
+
 function fetchUserInfo(userId){
     fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
         .then(response => {
-            console.log(response.status);
-
             if(!response.ok){
                 console.error("エラーレスポンス", response);
             }else{
                 return response.json()
                 .then(userInfo => {
-                    const view = escapeHTML`
-                    <h4>${userInfo.name} (@${userInfo.login})</h4>
-                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-                    <dl>
-                        <dt>Location</dt>
-                        <dd>${userInfo.location}</dd>
-                        <dt>Repositories</dt>
-                        <dd><${userInfo.poblic_repos}dd>
-                    </dl>
-                    `;
+                    const view = createView(userInfo)
 
-                    const result = document.getElementById("result");
-                    result.innerHTML = view;
+                    displayView(view)
                 });
             }
         }).catch(error => {
@@ -28,6 +20,25 @@ function fetchUserInfo(userId){
         });
     
 }
+
+function createView(userInfo){
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd><${userInfo.poblic_repos}dd>
+    </dl>
+    `;
+}
+
+function displayView(view){
+    const result = document.getElementById("result");
+    result.innerHTML = view;
+}
+
 
 
 function escapeSpecialChars(str) {
